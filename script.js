461c3d2616b07fae05d1b79a111a4b6002ddc44a
Steven <is.pena01@ciaf.edu.co>
2025-08-21 11:21:39 -0500

    estructura inicial del codigo

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 
 Books = []
+BorrowBook =[]
 
 class Book {
+    CreateBook(){
 
+    }
+
+    BorrowBook(){
+
+    }
+
+    ReturnBook(){
+
+    }
 }
 
 
 class Library {
+    AddBookToLibrary(){
+
+    }
+
+    RemoveBookToLibrary(){
+
+    }
+    CalculateFine(){
+
+    }
+    GetBookByGenere(){
+
+    }
+    GetOverdueBooks(){
+
+    }
+    GenerateLibraryReport(){
+        
+    }
 
 }
