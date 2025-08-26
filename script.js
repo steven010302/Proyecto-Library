6bbf0071186a47d38ba1147388c8f380c1ae32e8
Steven <is.pena01@ciaf.edu.co>
2025-08-26 00:44:57 -0500

    ajustes del proyecto

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
+ 
+// almacenar los datos (libros)
+const Books = [
+  CreateBook("Aliento de dragon", "Malena Salazar", "Fantasia", genetateISBN()),
+  CreateBook("Satanas", "Mario Mendoza", "Novela", genetateISBN()),
+  CreateBook("Historias Extraoridinarias", "Edgar Allan Poe", "Terror", genetateISBN()),
+];
 
-Books = []
-BorrowBook =[]
+// almacena los libros prestados
+const BorrowedBooks = new Map();
 
-class Book {
-    CreateBook(){
+// crear el isbn aleatoriamente
 
-    }
-
-    BorrowBook(){
-
-    }
-
-    ReturnBook(){
-
-    }
+function GenerateISBN(){
+  return `${Math.floor(Math.random()*100)+900}-${Math.floor(Math.random()*10)+90}-${Math.floor(Math.random()*1000)+9000}-${Math.floor(Math.random()*120)+480}-${Math.floor(Math.random()*10)}-`
 }
 
+// funcion para crear un libro 
 
-class Library {
-    AddBookToLibrary(){
-
-    }
-
-    RemoveBookToLibrary(){
-
-    }
-    CalculateFine(){
-
-    }
-    GetBookByGenere(){
-
-    }
-    GetOverdueBooks(){
-
-    }
-    GenerateLibraryReport(){
-        
-    }
-
+function CreateBook(Title, Author, Genere, ISBN){
+  return {
+    ID : Date.now() + math.floor(math.random()*1000), // importamos algunas funciones de math para evitar duplicados
+    Title,
+    Author,
+    Genere,
+    ISBN,
+    Is_Aviable : true,
+    BorrowedBy : null,
+    BorrowedAt : null,
+    DueDate : null,
+    CreatedAt : new Date()
+  };
 }
+
+// agregar un libro a la biblioteca
+
+function AddBookToLibrary(){
+  const Book =  CreateBook(Title, Author, Genere, ISBN)
+  BooksArray.push(Book)
+  return Book
+};
+
+// Eliminar un libro de la biblioteca
+
+function RemoveBookFromLibrary(BooksArray, ID){
+  const Index =  BooksArray.findIndex(Books => Books.ID ===ID)
+  if (Index != -1){
+    return BooksArray.splice(index , 1)[0]
+  };
+  
+  return null
+};
+
+
